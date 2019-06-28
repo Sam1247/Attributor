da550d51405da8b0e7e8118bfee93118cb33edc1
saman <abdallaelsaman67@gmail.com>
2019-06-28 18:42:20 +0200

    Initial Commit

---
+++ Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by Abdalla Elsaman on 6/28/19.
+//  Copyright © 2019 Dumbies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Attributor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by Abdalla Elsaman on 6/28/19.
+//  Copyright © 2019 Dumbies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
